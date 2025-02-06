a73e28c95ea15d95370d85808f184ebc3bc3e1f0
Yogananda1504 <vynr1504@gmail.com>
2025-02-06 20:23:36 +0530

    Modified the API Services for testing

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,4 +1,3 @@
-
 /// <reference types="vite/client" />
 // The above needs to be used in the frontend to access the env related variables 
 // This is the axios instance used for communicating with the backend
@@ -6,11 +5,17 @@
 import axios from 'axios';
 const url=import.meta.env.VITE_BACKEND;
 const authorization=import.meta.env.VITE_AUTHORIZATION;
-const base=axios.create({
-    baseURL:url,
-    headers:{
-        'authorization':authorization,
+const base = axios.create({
+    baseURL: "http://10.3.1.6:3000/",
+    headers: {
+        'authorization': "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOiI2NWQ3MjY2Y2M3MDU5ZTg4Yjc3YTM1MGYiLCJlbXBsb3llZUNvZGUiOiIxMDY3IiwiaWF0IjoxNzM4MzE0NDU4fQ.ay7KFLz8gTxIrcRt61IDQyNNRbz11wnxDhUS4UOlQoI",
+        'Content-Type': 'application/json',
+        // 'Accept': '*/*',
+        // 'Accept-Encoding': 'gzip, deflate, br',
+        // 'Connection': 'keep-alive'
     }
-})
+});
+
+
 
 export default base;
--- src/api/services/Student.ts
+++ src/api/services/Student.ts
@@ -2,10 +2,10 @@ import base from "../base";
 // These are the api functions which interact with the backend
 // They are used by the components to fetch and send data
 
-
 export const getOverallAttendance = async (scholarNumber: string) => {
-    const res = await base.post("/api/attendance/attendanceByScholarId", {
-        scholarNumber: scholarNumber
+    const response = await base.post("/api/attendance/attendanceByScholarId", {
+        scholarNumber
     });
-    return res.data;
-}
+    return response.data;
+};
+
