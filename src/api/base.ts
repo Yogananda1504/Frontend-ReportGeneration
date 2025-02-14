c30a0c59cd36a1b08b2aaeac091801be3454e922
Yogananda1504 <vynr1504@gmail.com>
2025-02-14 19:37:39 +0530

    Changed API

--- src/api/base.ts
+++ src/api/base.ts
@@ -16,6 +16,18 @@ const base = axios.create({
     }
 });
 
+export const local = axios.create({
+    baseURL: "http://localhost:3000/",
+    headers: {
+        // 'authorization': "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOiI2NWQ3MjY2Y2M3MDU5ZTg4Yjc3YTM1MGYiLCJlbXBsb3llZUNvZGUiOiIxMDY3IiwiaWF0IjoxNzM4MzE0NDU4fQ.ay7KFLz8gTxIrcRt61IDQyNNRbz11wnxDhUS4UOlQoI",
+        'Content-Type': 'application/json',
+        // 'Accept': '*/*',
+        // 'Accept-Encoding': 'gzip, deflate, br',
+        // 'Connection': 'keep-alive'
+    }
+});
+
+
 
 
 export default base;
--- src/api/services/Student.ts
+++ src/api/services/Student.ts
@@ -1,4 +1,5 @@
 import base from "../base";
+import { local } from "../base";
 // These are the api functions which interact with the backend
 // They are used by the components to fetch and send data
 
@@ -8,4 +9,9 @@ export const getOverallAttendance = async (scholarNumber: string) => {
     });
     return response.data;
 };
+
+export const getStudentDetails = async (scholarNumber : string ) =>{
+    const response = await local.post("/api/student/details",{scholarNumber})
+    return response.data;
+}
 
